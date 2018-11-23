71cd4bb5111cc3cfc8107705dfd7b1745048ba3b
DreamVinci <dreamvinci@qq.com>
2018-11-23 17:11:00 +0800

    add schedule

--- src/main/java/com/vinci/mycalendar/controller/ScheduleRestController.java
+++ src/main/java/com/vinci/mycalendar/controller/ScheduleRestController.java
@@ -4,6 +4,7 @@ import com.vinci.mycalendar.service.IScheduleService;
 import com.vinci.mycalendar.vo.Schedule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScheduleRestController {
     @Autowired
     private IScheduleService scheduleService;
-
+    @PostMapping("/addSchedule")
     public void addSchedule(Schedule schedule){
-
+    //test
+        scheduleService.addSchedule(schedule);
     }
 }
--- src/main/java/com/vinci/mycalendar/service/impl/ScheduleServiceImpl.java
+++ src/main/java/com/vinci/mycalendar/service/impl/ScheduleServiceImpl.java
@@ -4,10 +4,12 @@ import com.vinci.mycalendar.repository.ScheduleRepository;
 import com.vinci.mycalendar.service.IScheduleService;
 import com.vinci.mycalendar.vo.Schedule;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by yux on 2018/10/25.
  */
+@Service("scheduleService")
 public class ScheduleServiceImpl implements IScheduleService{
     @Autowired
     private ScheduleRepository dao;
